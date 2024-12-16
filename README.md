639ca9be7a46a60881029f19ef1dccd3c0b16e3f
KeelerZoroth <isaiah1hansen@gmail.com>
2024-12-16 14:14:36 -0600

    finished

---
+++ README.md
@@ -0,0 +1,42 @@
+# Social Network API
+## Description
+  This is a backend application using MongoDB and Node.js with a user, thought, and reaction schema including the proper routes to preform CRUD operations.
+## Table of Contents
+  [Description](#description)  
+  [Installation](#installation)  
+  [Usage](#usage)  
+  [License](#license)  
+  [Contributing](#contributing)  
+  [Tests](#tests)  
+  [Questions](#questions)  
+## Installation
+  Prerequisites: Node.js, npm  
+
+
+  run ```npm i```
+## Usage
+  run ```npm run start```  
+
+then send some http requests to:  
+/api/users  
+/api/users/:userId  
+/api/users/:userId/friends/:friendId  
+/api/thoughts  
+/api/thoughts/:thoughtId
+/api/thoughts/:thoughtId/reactions  
+
+link to HowToUse video: https://app.screencastify.com/v3/watch/9h48FMaYM5isJdsvexmj
+## License
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  
+## Contributing
+  1. Fork the repository.  
+  2. Create a new branch: ``` git checkout -b feature/branch-name ```  
+  3. Make your changes.  
+  4. Submit a pull request.
+## Tests
+  there are no tests
+## Questions
+  For any questions, contact here:  
+  Isaiah Hansen: isaiah1hansen@gmail.com  
+  GitHub: [KeelerZoroth](https://github.com/KeelerZoroth)  
--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -1,43 +1,50 @@
-// import connection from '../config/connection.js';
-// import { User, Thought } from '../models/index.js';
-// import { getRandomName, getRandomThoughts } from './data.js';
+import connection from '../config/connection.js';
+import { User, Thought } from '../models/index.js';
+import { getRandomName, getRandomThoughts } from './data.js';
 
-// connection.on('error', (err) => err);
+connection.on('error', (err) => err);
 
-// connection.once('open', async () => {
-//   console.log('connected');
-//   // Delete the collections if they exist
-//   let thoughtCheck = await connection.db?.listCollections({ name: 'thoughts' }).toArray();
-//   if (thoughtCheck?.length) {
-//     await connection.dropCollection('thoughts');
-//   }
+connection.once('open', async () => {
+  console.log('connected');
+  // Delete the collections if they exist
+  let thoughtCheck = await connection.db?.listCollections({ name: 'thoughts' }).toArray();
+  if (thoughtCheck?.length) {
+    await connection.dropCollection('thoughts');
+  }
 
-//   let userCheck = await connection.db?.listCollections({ name: 'users' }).toArray();
-//   if (userCheck?.length) {
-//     await connection.dropCollection('users');
-//   }
+  let userCheck = await connection.db?.listCollections({ name: 'users' }).toArray();
+  if (userCheck?.length) {
+    await connection.dropCollection('users');
+  }
 
-//   const users = [];
-//   const thoughts = [];
+  const users = [];
+  const numOfUsersToMake = 20;
 
-//   for (let i = 0; i < 20; i++) {
-//     const username = getRandomName();
-//     const splitName = username.split(" ");
-//     users.push({
-//       username,
-//       email: splitName[0] + i + splitName[1] + "@email.com",
-//     });
-//   }
+  for (let i = 0; i < numOfUsersToMake; i++) {
+    const username = getRandomName();
+    const splitName = username.split(" ");
+    users.push({
+      username,
+      email: splitName[0] + i + splitName[1] + "@email.com",
+    });
+  }
 
-//   await User.insertMany(users);
-//   await Thought.insertMany(thoughts);
+  const dbUsers = await User.insertMany(users);
+  
+  
+  
+  for(let nextUserIndex in dbUsers){
+    
+    const newUserThoughts = getRandomThoughts(Math.floor(Math.random() * 5), dbUsers[nextUserIndex].username);
+    
+    const arrDBThoughts = await Thought.insertMany(newUserThoughts);
+    
+    await User.findByIdAndUpdate(dbUsers[nextUserIndex]._id, { thoughts: (arrDBThoughts.map((nextThoughtElement) => nextThoughtElement._id))});
+  }
 
-//   // loop through the saved videos, for each video we need to generate a video response and insert the video responses
-//   console.table(users);
-//   console.table(thoughts);
-//   console.info('Seeding complete! 🌱');
-//   process.exit(0);
-// });
-
-
-// unfinished
+  
+  // loop through the saved videos, for each video we need to generate a video response and insert the video responses
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
